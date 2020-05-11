98222e002631e5d7e7d686cc63d4bfab1ad4b6be
DRK-Jingles <drk_jingles@windowslive.com>
2020-05-11 16:22:37 +1200

    initial creation of game screen - no moving parts

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -7,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
 import java.awt.BasicStroke;
+
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -15,26 +16,36 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	
 	private final static Color BACKGROUND_COLOUR = Color.black;
 	private final static int TIMER_DELAY = 5;
-	boolean gameInitialised = false;
-	Ball ball;
 	
-	public void createObjects() {
-		ball = new Ball(getWidth(), getHeight());
-	}	
-	private void update() {
-		if(!gameInitialised) {
-			createObjects();
-			gameInitialised = true;
-		}
-	}
-	private void paintSprite(Graphics g, Sprite sprite) {
-		g.setColor(sprite.getColour());
-		g.fillRect(sprite.getXPosition(),sprite.getYPosition(),sprite.getWidth(),sprite.getHeight());
-	}
+	GameState gameState = GameState.Initialising;
+	
+	Ball ball;
+	Paddle paddle1, paddle2;
+	
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOUR);
 		Timer timer = new Timer(TIMER_DELAY, this);
 		timer.start();
+	}
+	public void createObjects() {
+		ball = new Ball(getWidth(), getHeight());
+		paddle1 = new Paddle(Player.One, getWidth(), getHeight());
+		paddle2 = new Paddle(Player.Two, getWidth(), getHeight());
+	}	
+	private void update() {
+		switch(gameState) {
+          case Initialising: {
+            createObjects();
+            gameState = GameState.Playing;
+            break;
+          }
+          case Playing: {
+            break;
+          }
+          case GameOver: {
+            break;
+          }
+		}
 	}	
 	private void paintDottedLine(Graphics g) {
 	      Graphics2D g2d = (Graphics2D) g.create();
@@ -43,15 +54,11 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	         g2d.setPaint(Color.WHITE);
 	         g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
 	         g2d.dispose();
-	 }	
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		paintDottedLine(g);
-		if(gameInitialised) {
-			paintSprite(g, ball);
-		}
-	}
+	}	
+	private void paintSprite(Graphics g, Sprite sprite) {
+		g.setColor(sprite.getColour());
+		g.fillRect(sprite.getXPosition(),sprite.getYPosition(),sprite.getWidth(),sprite.getHeight());
+	}	
 	@Override
 	public void keyPressed(KeyEvent e) {
 	}
@@ -65,5 +72,15 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	public void actionPerformed(ActionEvent event) {
 		update();
 		repaint();		
+	}
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		paintDottedLine(g);
+		if(gameState != GameState.Initialising) {
+			paintSprite(g, ball);
+			paintSprite(g, paddle1);
+			paintSprite(g, paddle2);
+		}
 	}
 }
