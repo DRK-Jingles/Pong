12573850ce69eabfdfd23a10300139088aadcb35
DRK-Jingles <drk_jingles@windowslive.com>
2020-05-11 15:48:18 +1200

    addition of ball to pongpanel

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -15,13 +15,27 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	
 	private final static Color BACKGROUND_COLOUR = Color.black;
 	private final static int TIMER_DELAY = 5;
+	boolean gameInitialised = false;
+	Ball ball;
 	
+	public void createObjects() {
+		ball = new Ball(getWidth(), getHeight());
+	}	
+	private void update() {
+		if(!gameInitialised) {
+			createObjects();
+			gameInitialised = true;
+		}
+	}
+	private void paintSprite(Graphics g, Sprite sprite) {
+		g.setColor(sprite.getColour());
+		g.fillRect(sprite.getXPosition(),sprite.getYPosition(),sprite.getWidth(),sprite.getHeight());
+	}
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOUR);
 		Timer timer = new Timer(TIMER_DELAY, this);
 		timer.start();
-	}
-	
+	}	
 	private void paintDottedLine(Graphics g) {
 	      Graphics2D g2d = (Graphics2D) g.create();
 	         Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
@@ -29,40 +43,27 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	         g2d.setPaint(Color.WHITE);
 	         g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
 	         g2d.dispose();
-	 }
-	
+	 }	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		paintDottedLine(g);
+		if(gameInitialised) {
+			paintSprite(g, ball);
+		}
 	}
-
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
-
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
-
 	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void keyTyped(KeyEvent e) {		
 	}
-	private void update() {
-		
-	}
-
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		update();
-		repaint();
-		
+		repaint();		
 	}
-
 }
